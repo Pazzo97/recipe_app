265e50ebb0b895ebf58209ded0b789565370c650
Pazzo97 <pazzomuk@gmail.com>
2022-08-31 22:05:36 +0200

    Add food controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+ # Defines the root path route ("/")
+ root "home#index"
+
   devise_scope :user do # This block of code is mandatory for devise to support logout in rails 7
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
-  devise_for :users  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+ 
+  resources 'foods', only: %w[index new create destroy] 
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
