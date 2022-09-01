2b6256ad70948364c1ea886c94c2f99951f906d5
Pazzo97 <pazzomuk@gmail.com>
2022-09-01 09:59:50 +0200

    Recipe controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_food, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :preparation_time, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 1 }
+  validates :cooking_time, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 1 }
+  validates :description, presence: true, allow_blank: true
+  validates :public, inclusion: { in: [true, false] }
+
 end
